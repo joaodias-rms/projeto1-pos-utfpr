6b66e437fff6dd0fc784daef3350bc6e6778c405
João Ramos <jvd.ramos@hotmail.com>
2026-05-23 07:06:06 -0400

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,127 @@
+package com.example.myapplication
+
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import kotlin.math.pow
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var pesoInput: EditText
+
+    private lateinit var alturaInput: EditText
+
+    private lateinit var imcResult: TextView
+
+    private lateinit var calcular: Button
+
+    private lateinit var limpar: Button
+
+    private lateinit var idadeInput: EditText
+
+    private lateinit var tmbResult: TextView
+
+    private lateinit var statusImcResult: TextView
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        pesoInput = findViewById(R.id.peso_input)
+
+        alturaInput = findViewById(R.id.altura_input)
+
+        imcResult = findViewById(R.id.imcResult)
+
+        calcular = findViewById(R.id.calcular)
+
+        limpar = findViewById(R.id.limpar)
+
+        tmbResult = findViewById(R.id.tmbResult)
+
+        idadeInput = findViewById(R.id.idade_input)
+
+        statusImcResult = findViewById(R.id.statusImcResult)
+
+        calcular.setOnClickListener {
+            calcularIMC()
+            calcularTaxaMetabolicaBasal()
+        }
+
+        calcular.setOnLongClickListener{
+            Toast.makeText(
+                this,
+                "Botão para calcular o IMC",
+                Toast.LENGTH_SHORT)
+                .show()
+            true
+        }
+
+        limpar.setOnClickListener{
+           limparInput()
+        }
+
+    }
+
+    private fun limparInput() {
+        pesoInput.setText("")
+        alturaInput.setText("")
+        idadeInput.setText("")
+        statusImcResult.text = ""
+        imcResult.text = getString(R.string._0_0)
+        tmbResult.text = getString(R.string.tmb_0_0)
+        pesoInput.requestFocus()
+    }
+
+    private fun calcularIMC() {
+        val peso = pesoInput.text.toString().toDoubleOrNull()
+        val altura = alturaInput.text.toString().toDoubleOrNull()
+        if (peso != null && altura != null) {
+            val imc = peso / altura.pow(2)
+            imcResult.text = String.format("%.2f", imc)
+            val classificacao = when {
+                imc < 18.5 -> "Abaixo do peso"
+                imc < 25.0 -> "Peso normal"
+                imc < 30.0 -> "Sobrepeso"
+                imc < 35.0 -> "Obesidade Grau I"
+                imc < 40.0 -> "Obesidade Grau II"
+                else -> "Obesidade Grau III"
+            }
+
+            statusImcResult.text = classificacao
+        }
+    }
+
+    private fun calcularTaxaMetabolicaBasal() {
+        val peso = pesoInput.text.toString().toDoubleOrNull()
+        val altura = alturaInput.text.toString().toDoubleOrNull()
+        val idade = idadeInput.text.toString().toIntOrNull()
+        if(peso == null ){
+            pesoInput.error = "Peso deve ser preeenchido"
+        }
+        if(altura == null){
+            alturaInput.error = "Altura deve ser preenchida"
+        }
+        if(idade == null){
+            idadeInput.error = "Idade deve ser preenchida"
+        }
+        if (peso != null && altura != null && idade != null) {
+            val tmb = 88.36 + (13.4 * peso) + (4.8 * altura) - (5.7 * idade)
+            tmbResult.text = String.format("%.2f", tmb).plus(" kcal/dia")
+        }
+    }
+}
